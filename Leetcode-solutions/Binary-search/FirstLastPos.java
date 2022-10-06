5267ce5f74d8ec45723219b5fcda80afb2ee2bd6
Sitam Meur <103279526+sitamgithub-MSIT@users.noreply.github.com>
2022-10-06 14:49:16 +0530

    FirstLastPos.py
    This is the python version of the above solution of the Leetcode question "34. Find First and Last Position of Element in Sorted Array".

--- Leetcode-solutions/Binary-search/FirstLastPos.java
+++ Leetcode-solutions/Binary-search/FirstLastPos.java
@@ -1,47 +1,26 @@
-import java.util.Arrays;
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
+        left=self.binarysearch(nums,target,True)
+        right=self.binarysearch(nums,target,False)
+        
+        return([left,right])
 
-public class FirstLastPos {
-    public static void main(String[] args) {
-        int[] nums = {5,7,7,8,8,10};
-        int target = 8;
-        int[] ans = firstLastPosition(nums, target);
-        System.out.println( Arrays.toString(ans) );
-    }
-
-    static int[] firstLastPosition(int[] nums, int target){
-        int[] ans = {-1, -1};
-
-        ans[0] = serach(nums, target, true);
-        if(ans[0] == -1){
-            return ans;
-        }
-        ans[1] = serach(nums, target, false);
-        return ans;
-    }
-
-    static int serach(int[] nums, int target, boolean findingFirst){
-        int s = 0;
-        int e = nums.length-1;
-        int ans = -1;
-
-        while(s <= e){
-            int m = s + (e-s)/2;
-
-            if(nums[m] == target){
-                ans = m;
-
-                if( findingFirst ){
-                    e = m-1;
-                } else{
-                    s = m+1;
-                }
-            } else if(nums[m] < target){
-                s = m+1;
-            } else{
-                e = m-1;
-            }
-        }
-
-        return ans;
-    }
-}
+        
+    def binarysearch(self,nums,target,leftbias):   
+        l,r=0,len(nums)-1
+        i=-1
+        while(l<=r):
+            mid=(l+r)//2
+            if(nums[mid]>target):
+                r=mid-1
+            elif(nums[mid]<target):
+                l=mid+1
+            else:
+                i=mid
+                if leftbias:
+                    r=mid-1
+                else:
+                    l=mid+1
+        return(i)
+    
